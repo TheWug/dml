503d628881f9d8f25d684ec3602c028c4472006b
Ryan S <wuuug@telegram>
2021-07-23 04:27:33 -0700

    Add a few more structural interfaces.
    + ScanInto (empty interface, used for readability)
    + GetFields
    + ScanIntoPostProcessable
    
    Also add nilScanner type, implementing sql.Scanner, which performs a no-op scan.

---
+++ scanfrom.go
@@ -0,0 +1,37 @@
+package dml
+
+// Any struct containing `dml` field tags is considered to be a ScanInto, and can be the subject of a Scan.
+// structs which are processed by this package use field tags similarly to how the `json` package uses them.
+// those tags are used to determine a mapping by which a structure's fields are populated by a Scannable.
+//
+// see the following example:
+// type Example struct {                 // fields will be populated from Scannable columns:
+//     Id        int `dml:"table_id"`    // table_id
+//     TestField int `dml:"table_field"` // table_field
+// }
+//
+// Remember that each field in the scannable output can only output its value once. However, multiple fields
+// in the output may have the same name. Tagged fields, from top to bottom, will be mapped to fields
+// appearing in the output, from left to right. For operations which process multiple objects, this allows
+// you to marshal data from a single row into several objects of the same type.
+type ScanInto interface{}
+
+// There also exists the following interface, which allows you to provide your own GetFields implementation
+// for a struct.  You can use this to provide field wrappers, multi-level field population, and other more
+// complex features in an explicit way.
+type GetFields interface {
+	GetFields() (NamedFields, error)
+}
+
+// Some structs may choose to store their field values in the database in some sort of intermediate format.
+// those structs can implement ScanIntoPostProcessable, to receive an extra callback after the scan finishes,
+// to assist with converting between formats.
+type ScanIntoPostProcessable interface {
+	PostScan() error
+}
+
+// nilScanner is an internal type which is used to discard fields from queries which are not requested.
+type noopScanner struct {}
+
+// nilScanner.Scan() simply discards its input.
+func (n noopScanner) Scan(interface{}) error { return nil }
